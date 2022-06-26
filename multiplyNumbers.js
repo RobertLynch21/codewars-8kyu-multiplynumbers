baa002883fcd3e17b1b83f407b368b048d5b66d6
Robert Lynch <r.f.lynch@gmail.com>
2022-06-26 11:57:38 -0400

    codewars 8kyu multiplying 2 numbers

---
+++ multiplyNumbers.js
@@ -0,0 +1,11 @@
+// Implement a function which multiplies two numbers.
+
+// Solution
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+let resMultiply = multiply(3, 2)
+
+console.log(resMultiply)
